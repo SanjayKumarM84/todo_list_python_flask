336770e362c38c3cdcbe26d44b9aeb48fa837777
SanjayKumarM84 <sanjaymuniyappa1967@gmail.com>
2022-12-07 17:07:31 +0530

    Boiler Plates Created

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:password@localhost:5432/todolist"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/home',methods=['GET'])
 def home():
